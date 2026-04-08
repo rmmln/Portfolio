c3aa22e75f05c513cf24458579616b3c4f10bc48
rmmln <rmmoleno1@gmail.com>
2026-04-08 18:42:28 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className={`m-auto gap-2 flex flex-col min-h-screen items-center justify-start transition-colors duration-300 ${isDarkMode ? 'bg-gradient-to-br from-black via-gray-800 to-gray-900' : 'bg-gradient-to-br from-white via-gray-50 to-gray-100'}`}>
-      <div className={`mt-2 relative flex justify-between items-center w-full max-w-4xl backdrop-blur-xl border rounded-2xl p-5 shadow-4xl transition-colors duration-300 ${isDarkMode ? 'bg-white/10 border-white/20' : 'bg-black/10 border-black/20'}`}>
+      <div className="mt-2 relative flex justify-between items-center w-full max-w-4xl transition-colors duration-300">
         <button 
           onClick={toggleTheme}
           className={`absolute top-5 right-5 inline-flex items-center h-6 w-11 rounded-full transition-colors duration-300 p-0.5 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}
@@ -42,16 +42,16 @@ function App() {
             cursor={true}
           />
         </h1>
-        <p className={`text-xs font-inter font-light transition-colors duration-300 ${isDarkMode ? 'text-white' : 'text-gray-700'}`}>
+        <p className={`text-xs font-inter font-md transition-colors duration-300 ${isDarkMode ? 'text-white' : 'text-gray-700'}`}>
           <img src={location} alt="Location" className={`w-4 h-4 inline-block transition-all duration-300 ${isDarkMode ? 'opacity-100' : 'brightness-0'}`} />
           Cavite, Philippines
         </p>
-      <p className={`text-sm font-inter font-light transition-colors duration-300 ${isDarkMode ? 'text-white' : 'text-gray-700'}`}>
+      <p className={`text-sm font-inter font-md transition-colors duration-300 ${isDarkMode ? 'text-white' : 'text-gray-700'}`}>
           IT Student | UI/UX Designer 
         </p>
         <div className="flex w-full gap-2 items-center">
         <a href={cvPdf} download="MolenoCV.pdf" target="_blank" rel="noopener noreferrer" className={`text-xs font-inter font-semibold py-1 px-6 rounded-sm transition duration-300 ease-in-out transform hover:scale-105 ${isDarkMode ? 'bg-white text-gray-900 hover:bg-gray-100' : 'bg-black text-white hover:bg-gray-800'}`}>
-          View CV
+          Download CV
         </a>
         <div className="flex flex-row gap-1 justify-start">
           <a href="https://www.facebook.com/rmoleno15" target="_blank" rel="noopener noreferrer">
@@ -71,17 +71,17 @@ function App() {
       </div>
     </div>
   </div>
-  <div className="flex flex-row items-center  gap-2  w-full max-w-4xl">
-  <div className={`w-full max-w-xl backdrop-blur-xl border rounded-2xl p-5 shadow-4xl transition-colors duration-300 ${isDarkMode ? 'bg-white/10 border-white/20' : 'bg-black/10 border-black/20'}`}> 
+  <div className="flex flex-row items-stretch  gap-2  w-full max-w-4xl">
+  <div className={`w-1/3 max-w-xl backdrop-blur-xl border rounded-2xl p-5 shadow-4xl transition-colors duration-300 ${isDarkMode ? 'bg-white/10 border-white/20' : 'bg-white border-gray-200'}`}> 
       <h1 className={`text-xl font-bold transition-colors duration-300 ${isDarkMode ? 'text-white' : 'text-gray-900'}`}>Education</h1>
-      <ul className={`list-disc text-xs font-inter pl-5 transition-colors duration-300 ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>
+      <ul className={` leading-5 list-disc text-xs font-inter pl-5 transition-colors duration-300 ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>
         <li>Bachelor of Science in Information Technology - CVSU (2022-Present)</li>
         <li>Technical Vocational Computer Programming - LYFJSHS (2018-2020)</li>
         <li>Governor Ferrer Memorial National High School - Main (2014-2018)</li>
         <li>General Artemio Ricarte Memorial School - (2008-2014)</li>
         </ul>
     </div>
-    <div className={`w-full max-w-3xl backdrop-blur-xl border rounded-2xl p-5 shadow-4xl transition-colors duration-300 ${isDarkMode ? 'bg-white/10 border-white/20' : 'bg-black/10 border-black/20'}`}>
+    <div className={`  w-2/3 max-w-full backdrop-blur-xl border rounded-2xl p-5 shadow-4xl transition-colors duration-300 ${isDarkMode ? 'bg-white/10 border-white/20' : 'bg-white border-gray-200'}`}>
       <h1 className={`text-2xl font-bold transition-colors duration-300 ${isDarkMode ? 'text-white' : 'text-gray-900'}`}>About</h1>
         <p className={`w-full max-w-full mt-2 text-sm font-inter leading-5 text-justify transition-colors duration-300 ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>
                 Im an aspiring UI/UX designer passionate about creating clean,
