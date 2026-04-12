ba776ad289ee96215e4f34ef395b3a92a87dd426
rmmln <rmmoleno1@gmail.com>
2026-04-12 17:09:36 +0800

    Revert "feat: initial commit"
    This reverts commit cb7937b0fae5b819e335a163ae327c702cc76ca7.

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { fileURLToPath } from 'url'
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-
-// Define __dirname for ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [
+    tailwindcss(),
+  ],
 })
