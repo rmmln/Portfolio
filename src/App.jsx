064ffa3166c6ae5bc602bfe3ec1bc2aeeda065db
rmmln <rmmoleno1@gmail.com>
2026-04-21 23:25:19 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
               sequence={["ROBERT MOLENO", 2000, "", 50]}
               speed={50}
               repeat={Infinity}
-              cursor={true}
+              cursor={false}
             />
           </span>
         </h1>
